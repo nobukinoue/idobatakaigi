7ced7ed2f630517b77b8282ff1d1a35a8718b49a
nobukinoue <inoue@covvy.dev>
2022-02-16 14:40:04 +0900

    日本語入力確認

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -52,7 +52,8 @@ export default function SignIn({ setName }) {
   const classes = useStyles();
   const [disabled, setDisabled] = useState(true);
   const [string, setString]= useState('');
-  console.log({disabled, string});
+  const [isComposed, setIsComposed]= useState(false);
+  console.log({disabled, string, isComposed});
 
   useEffect(() => {
       const disabled = string === ''
@@ -79,11 +80,15 @@ export default function SignIn({ setName }) {
             autoFocus
             onChange={(e) => setString(e.target.value)}
             onKeyDown={(e) => {
+                if (isComposed) return;
                 if (e.key === 'Enter'){
                     setName(e.target.value);
                     e.preventDefault();
                 }
             }}
+            onCompositionStart={() => setIsComposed(true)}
+            onCompositionEnd={() => setIsComposed(false)}
+            
           />
           
           
