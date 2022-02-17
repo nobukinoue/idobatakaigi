bc3831c5b7118d25e0db35ec6ebb69699ecc1d28
nobukinoue <inoue@covvy.dev>
2022-02-17 13:37:44 +0900

    env-setting

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,24 @@
 import firebase from "firebase";
-  
+
+console.log(process.env)
+const {
+REACT_APP_FIREBASE_API_KEY,
+REACT_APP_FIREBASE_AUTH_DOMAIN,
+REACT_APP_FIREBASE_PROJECTID,
+REACT_APP_FIREBASE_STORAGEBUCKET,
+REACT_APP_FIREBASE_MESSAGINGSENDERID,
+REACT_APP_FIREBASE_APPID,
+REACT_APP_FIREBASE_DATABASEURL,
+} =process.env;
   
   const firebaseConfig = {
-    apiKey: "AIzaSyBQV6tXcjSnsrG_0TevkoApx-hZPRiSHzw",
-    authDomain: "idobatakaigi-with-ham-5da73.firebaseapp.com",
-    projectId: "idobatakaigi-with-ham-5da73",
-    storageBucket: "idobatakaigi-with-ham-5da73.appspot.com",
-    messagingSenderId: "624910757331",
-    appId: "1:624910757331:web:a2105d0f535851165e9326",
-    databaseURL: "https://idobatakaigi-with-ham-5da73-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    apiKey: REACT_APP_FIREBASE_API_KEY,
+    authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: REACT_APP_FIREBASE_PROJECTID,
+    storageBucket: REACT_APP_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: REACT_APP_FIREBASE_MESSAGINGSENDERID,
+    appId: REACT_APP_FIREBASE_APPID,
+    databaseURL: REACT_APP_FIREBASE_DATABASEURL,
   };
  
   firebase.initializeApp(firebaseConfig);
