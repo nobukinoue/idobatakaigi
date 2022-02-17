c204724cc7df9794791d1115aed0228f8603ecda
nobukinoue <inoue@covvy.dev>
2022-02-17 14:59:12 +0900

    just-focusing

--- src/components/MessageField.js
+++ src/components/MessageField.js
@@ -2,15 +2,15 @@ import React, { useState } from "react";
 import { TextField } from '@material-ui/core';
 import { pushMessage } from "../firebase";
 
-const MessageField = ({ name, setText, text }) => {
+const MessageField = ({ inputEl, name, setText, text }) => {
     const [isComposed, setIsComposed] = useState(false);
     return (
     <TextField
+            autoFocus
             fullWidth={true}
-            onChange={(e) => {
-                setText(e.target.value);
-            }}
-            onKeyDown={(e) => {
+            inputRef={inputEl}
+            onChange={(e) => setText(e.target.value)}
+             onKeyDown={(e) => {
                 if (isComposed) return;
 
                 const text = e.target.value;
--- src/components/MessageSubmitButton.js
+++ src/components/MessageSubmitButton.js
@@ -1,16 +1,17 @@
-import { IconButton } from "@material-ui/core";
+import { IconButton, } from "@material-ui/core";
 import SendIcon from '@material-ui/icons/Send';
 import React from "react";
 
 import { pushMessage } from '../firebase';
 
-const MessageSubmitButton = ({ name, setText, text }) => {
+const MessageSubmitButton = ({ inputEl, name, setText, text }) => {
     return (
         <IconButton disabled={text === ''}
             onClick={() => {
                 pushMessage({ name, text });
                 setText('');
-            }}
+                inputEl.current.focus();
+;            }}
             >
             <SendIcon />
             </IconButton>
